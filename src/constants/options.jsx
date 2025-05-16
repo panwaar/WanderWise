5fd43ef8e268e0c8e184a28259fb2f5f0dc91ae3
panwaar <panwarrahul3183@gmail.com>
2025-05-16 19:12:33 +0530

    updated create-trip and footer section

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -2,28 +2,28 @@ export const SelectTravelList = [
     {
         id:1,
         title: 'Just Me',
-        desc: 'A sole traveles in exploration',
+        desc: 'A lone traveler sets out to explore',
         icon: '✈️',
         people:'1 person'
     },
     {
         id:2,
         title: 'A Couple',
-        desc: 'Two traveles in tandem',
+        desc: 'Exploring together, creating memories',
         icon: '🥂',
         people:'2 people'
     },
     {
         id:3,
         title: 'Family',
-        desc: 'A group of fun loving adv',
+        desc: 'A group of fun-loving adventurers',
         icon: '🏡',
         people:'3 to 5 People'
     },
     {
         id:4,
         title: 'Friends',
-        desc: 'A bunch of thrill-seekes',
+        desc: 'A bunch of thrill-seekers',
         icon: '⛵',
         people:'5 to 10 people'
     }
@@ -33,19 +33,19 @@ export const SelectBudgetOptions = [
     {
         id:1,
         title: 'Cheap',
-        desc: 'Stay conscious of costs',
+        desc: 'Travel smart, spend less',
         icon: '💵',
     },
     {
         id:2,
         title: 'Moderate',
-        desc: 'Keep cost on the average side',
+        desc: 'Balanced comfort without breaking the bank',
         icon: '💰',
     },
     {
         id:3,
         title: 'Luxury',
-        desc: 'Dont worry about cost',
+        desc: 'Where comfort meets elegance',
         icon: '💸',
     }
 ]
